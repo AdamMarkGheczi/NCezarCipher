1cb57afa0165d223733b4634d5e178fff9c106e1
Adam Gheczi <adamark03@gmail.com>
2023-10-08 19:54:54 +0300

    First commit

---
+++ NCezar.cs
@@ -0,0 +1,85 @@
+﻿namespace NCezar
+{
+    internal class NCezar
+    {
+        private string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        public void SetCustomAlphabet(string alphabet)
+        {
+            this.alphabet = alphabet;
+        }
+
+        public string Encrypt(string input, int n)
+        {
+            string output = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int index = alphabet.IndexOf(Char.ToLower(input[i]));
+                char currentChar = input[i];
+
+                if (index != -1)
+                {
+                    currentChar = alphabet[(index + n) % alphabet.Length];
+
+                    if (char.IsUpper(input[i])) currentChar = char.ToUpper(currentChar);
+                }
+
+                output += currentChar;
+            }
+
+            return output;
+        }
+
+        public string Decrypt(string input, int n)
+        {
+            string output = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int index = alphabet.IndexOf(Char.ToLower(input[i]));
+                char currentChar = input[i];
+
+                if (index != -1)
+                {
+                    currentChar = alphabet[(index - n % alphabet.Length + alphabet.Length) % alphabet.Length];
+
+                    if (char.IsUpper(input[i])) currentChar = char.ToUpper(currentChar);
+                }
+
+                output += currentChar;
+            }
+
+            return output;
+        }
+
+        public int Cryptanalisys(string input)
+        {
+            string Dictionary = File.ReadAllText(@"../../../dictionary.txt");
+
+            for (int i = 0; i < 26; i++)
+            {
+                string test = Decrypt(input, i).ToLower();
+                char[] separators = new char[] { ' ', '!', '.', ',', '?', ';' };
+                string[] words = test.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                int count = 0;
+                foreach(string word in words)
+                {
+                    
+                    if(Dictionary.Contains(word)) count++;
+                }
+
+                float successRate = ((float)count / words.Length) * 100;
+
+                if(successRate >= 80)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace NCezar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello World!";
+
+            NCezar cezar = new NCezar();
+
+            Console.WriteLine(text);
+
+            string encr = cezar.Encrypt(text, 5);
+            Console.WriteLine(encr);
+
+            Console.WriteLine(cezar.Cryptanalisys(encr));
+        }
+    }
+}
